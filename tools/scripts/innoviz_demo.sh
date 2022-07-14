bfc9e5956010a810dca8977a0e25145044dc2353
markk <markk@innoviz-tech.com>
2022-07-14 21:45:55 +0300

    innoviz_dataset: restore demo

--- tools/scripts/innoviz_demo.sh
+++ tools/scripts/innoviz_demo.sh
@@ -1,6 +1,6 @@
 # run demo with dummy model.
 # Dummy model - generating dummy detections by adding noise to GT data.
-# python innoviz_demo.py --cfg_file cfgs/innoviz_models/dummy.yaml --data_path ../data/innoviz/testing
+python innoviz_demo.py --cfg_file cfgs/innoviz_models/dummy.yaml --data_path ../data/innoviz/training
 
 # demo for running an arbitrary model. results are expected to be arbitrary.
 # python innoviz_demo.py --cfg_file cfgs/innoviz_models/pv_rcnn.yaml --ckpt pv_rcnn_8369.pth --data_path ../data/innoviz/testing
